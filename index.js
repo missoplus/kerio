f416277046013ec8ae2bf97aa32e3c9408cad1e5
nill <111270272+missoplus@users.noreply.github.com>
2022-11-25 21:40:06 +0330

    add panel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express=require('express')
+const express = require('express');
 const logger = require('morgan');
 const jwtSecret = require('./config/jwt')
 const bodyParser = require('body-parser');
@@ -8,9 +8,24 @@ const groups = require('./routes/groups');
 const requestLogger = require('./middleware/request.middleware')
 const mongoose = require('./config/db'); //database configuration
 const jwtMiddleware = require('./middleware/jwt.middleware')
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const cors = require('cors');
+const bodParser = require('body-parser');
+const authRoutes = require('./routes/auth-routes');
+const homeRoutes = require('./routes/home-routes');
+const app = express();
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(bodParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(homeRoutes.routes);
+app.use(authRoutes.routes);
 // connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-const app=express()
+// const app=express()
 app.set('secretKey', 'nodeRestApi'); // jwt secret token
 // connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
